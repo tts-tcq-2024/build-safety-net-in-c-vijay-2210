052c86693c543e46a06a494a196d19ea85a71405
vijay-2210 <73438496+vijay-2210@users.noreply.github.com>
2024-07-05 08:14:40 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,45 +1,39 @@
-#include        <string.h> 
-#include        <ctype.h> 
-char *soundex(const char *in)
-{
-   static  int code[] =
-      {  0,1,2,3,0,1,2,0,0,2,2,4,5,5,0,1,2,6,2,3,0,1,0,2,0,2 };
-      
-   static  char key[5];
-   register        char ch;
-   register        int last;
-   register        int count;
-   
-   strcpy(key, "Z000");
-  
-   while (*in != '\0'  &&  !isalpha(*in))
-      ++in;
-   if (*in == '\0')
-      return key;
+#ifndef SOUNDEX_H
+#define SOUNDEX_H
 
-   key[0] = toupper(*in);
-   last = code[key[0] - 'A'];
-   ++in;
-   for (count = 1;  count < 4  &&  *in != '\0';  ++in) {
-      if (isalpha(*in)) {
-         ch = tolower(*in);
-         if (last != code[ch - 'a']) {
-            last = code[ch - 'a'];
-            if (last != 0)
-               key[count++] = '0' + last;
-         }
-      }
-   }
-   return key;
-}
-#ifdef  TESTPROG
+#include "Soundex.h"
+#include <ctype.h>
+#include <string.h>
 
-#include    <stdio.h>   
-int main()
-{
-   char    instring[80];
-   while (fgets(instring, sizeof instring, stdin) != NULL)
-      printf("%s\n", soundex(instring));
-   return 0;
+char getSoundexCode(char c) {
+    c = toupper(c);  
+        if(c== 'B' || c=='F' || c=='V'){return '1';}
+        if(c== 'C' || c=='G' || c=='J'||c=='K'|| c=='Q'|| c=='S'||c=='X'|| c=='Z' ){return '2';}
+        if(c== 'D' || c=='T' ){return '3';}
+        if(c== 'L' ){return '4';}
+        if(c== 'M' || c=='N' ){return '5';}
+        if(c== 'R'){return '6';}
+        else{return '0';} 
+    }
+
+
+void generateSoundex(const char *name, char *soundex) {
+    int len = strlen(name);
+    soundex[0] = toupper(name[0]);
+    int sIndex = 1;
+
+    for (int i = 1; i < len && sIndex < 4; i++) {
+        char code = getSoundexCode(name[i]);
+        if (code != '0' && code != soundex[sIndex - 1]) {
+            soundex[sIndex++] = code;
+        }
+    }
+
+    while (sIndex < 4) {
+        soundex[sIndex++] = '0';
+    }
+
+    soundex[4] = '\0';
 }
-#endif  TESTPROG
+
+#endif // SOUNDEX_H
